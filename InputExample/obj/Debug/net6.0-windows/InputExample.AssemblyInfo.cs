ce8bf782ef8fddc566d6594846a975c2fcf4b2d8
Synoeca <synoeca523@ksu.edu>
2024-08-23 17:12:45 -0500

    Push remain changes

--- InputExample/obj/Debug/net6.0-windows/InputExample.AssemblyInfo.cs
+++ InputExample/obj/Debug/net6.0-windows/InputExample.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InputExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+631a5eed8b15f81dc9d6fa698baf6a80f09b83b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+767f692e67d5f5575ba7637ad3c886dc953d9c2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("InputExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InputExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
