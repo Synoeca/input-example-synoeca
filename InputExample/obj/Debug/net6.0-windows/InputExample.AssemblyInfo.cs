8a17339b7e36a2a152d1293fb0b63020d0d257d4
Synoeca <synoeca523@ksu.edu>
2024-08-23 15:45:50 -0500

    Forked

--- InputExample/obj/Debug/net6.0-windows/InputExample.AssemblyInfo.cs
+++ InputExample/obj/Debug/net6.0-windows/InputExample.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InputExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f1c9e9de35f9392c37b63d1b3e56b979c95a173")]
 [assembly: System.Reflection.AssemblyProductAttribute("InputExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InputExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
